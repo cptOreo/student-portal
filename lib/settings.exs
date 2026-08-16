88f151d3325e8ab5c245d79ede120971d86c4a27
cptOreo <dhavla.ravi@gmail.com>
2026-08-16 21:00:32 +0530

    feat: add language validation to Settings

--- lib/settings.exs
+++ lib/settings.exs
@@ -12,6 +12,15 @@ defmodule Settings do
   def set_theme(settings, theme) when theme in ["light", "dark"] do
     %{settings | theme: theme}
   end
+  
+  @supported_languages ["en", "es", "fr", "de"]
+
+  def set_language(settings, lang) do
+    if lang in @supported_languages do
+      {:ok, %{settings | language: lang}}
+    else
+      {:error, "Unsupported language"}
+    end
+  end
 
 end
-
