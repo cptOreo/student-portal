28ffb29e77b3a360aca8a7b316ab472bd01c1d4a
cptOreo <dhavla.ravi@gmail.com>
2026-08-16 21:38:18 +0530

    feat: add letter_grade function to StudentPortal

--- lib/student-portal.exs
+++ lib/student-portal.exs
@@ -22,6 +22,18 @@ defmodule StudentPortal do
       [_head | tail] -> %{student | grades: tail}
     end
   end
+
+  def letter_grade(student) do
+    avg = average_grade(student)
+
+    cond do
+      avg >= 90 -> "A"
+      avg >= 80 -> "B"
+      avg >= 70 -> "C"
+      avg >= 60 -> "D"
+      true -> "F"
+    end
+  end
 end
 
 student = StudentPortal.new_student("Ravi", 4004)
