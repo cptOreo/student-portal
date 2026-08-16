0e16e9dcd74420114c0d0ad4f28b53efa7a85b4e
cptOreo <dhavla.ravi@gmail.com>
2026-08-16 19:58:46 +0530

    feat: add add_course and remove_course functions to the dashboard

--- lib/dashboard.exs
+++ lib/dashboard.exs
@@ -4,5 +4,14 @@ defmodule Dashboard do
   def new(students) do
     %Dashboard{students: students, courses: [], announcements: []}
   end
+
+  def add_course(%Dashboard{} = dashboard, course) do
+    %Dashboard{dashboard | courses: [course | dashboard.courses]}
+  end
+
+  def remove_course(%Dashboard{} = dashboard, course) do
+    %Dashboard{dashboard | courses: List.delete(dashboard.courses, course)}
+  end
+
 end
 
