15344d8d8245774500320d5baf8529266481202c
cptOreo <dhavla.ravi@gmail.com>
2026-08-16 19:40:15 +0530

    feat: add update_bio and update_email functions added to the profile

--- lib/profile.exs
+++ lib/profile.exs
@@ -4,5 +4,14 @@ defmodule Profile do
   def new(name, email, year) do
     %Profile{name: name, email: email, year: year}
   end
+
+  def update_bio(%Profile{} = profile, new_bio) do
+    %Profile{profile | bio: new_bio}
+  end
+
+  def update_email(%Profile{} = profile, new_email) do
+    %Profile{profile | email: new_email}
+  end
+
 end
 
